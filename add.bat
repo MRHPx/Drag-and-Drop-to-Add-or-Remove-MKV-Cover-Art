aa94c8b481d6911554c56f500ce2807addad3f0e
Moh Rizal HP <mrhpx@yahoo.com>
2019-03-30 23:00:39 +0700

    Add files via upload

---
+++ add.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+%~dp0/mkvpropedit %* --add-attachment "%~dp0cover.jpg" --add-attachment "%~dp0small_cover.jpg"
---
+++ remove.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+%~dp0/mkvpropedit %* --delete-attachment "1" --delete-attachment "2" --delete-attachment "3" --delete-attachment "4"
+
